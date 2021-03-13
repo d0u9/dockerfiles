b6901e216b06f5bca46bec9d26f65c29f08df514
d0u9 <d0u9.su@outlook.com>
2021-03-13 21:33:37 +0800

    update samba

--- samba/start.sh
+++ samba/start.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
+
 set -e
 
-if [[ "$*" == smb*start* ]]; then
-    base_dir="/etc/samba"
-    target_dir="$SMB_CONF_DIR"
+# USER SETTING ENV FORMAT
+# GROUP=<group1>[:GID1];<group2>[:GID2]
+# USER=<username1:passwd1>[:UID1:groupA,groupB];<username2;passwd2>[:UID2:groupA,groupC]
 
-    if [ -z "$(ls -A "$target_dir")" ]; then
-        cp -r $base_dir/* $target_dir/
-    fi
+# For test
+#group_str="user1:1000;user2:2000;public:5000;ano"
+#user_str="user1:passwd1:1000:user1,public;user2:passwd2:2000:user2,public,ano;public::5000:public"
 
-    rm -fr "$base_dir"
-    ln -s "$target_dir" "$base_dir"
+group_str=${GROUP:-"guest"}
+user_str=${USER:-"guest:badpassword"}
 
-    smbd
-    tail -f /dev/null
-fi
+# Create groups
+IFS=';' read -ra groups <<< "$group_str"; unset IFS
+for group in "${groups[@]}"; do
+    IFS=':'; arr=($group); unset IFS
+    groupname=${arr[0]}
+    gid=${arr[1]}
 
-exec "$@"
+    echo "group=$groupname, gid=$gid"
+    grep -q "$groupname:" /etc/group || addgroup ${gid:+-g "$gid"} $groupname
+done
+
+# Create users
+IFS=';' read -ra users <<< "$user_str"; unset IFS
+for user in "${users[@]}"; do
+    IFS=':'; arr=($user); unset IFS
+    username=${arr[0]}
+    passwd=${arr[1]}
+    uid=${arr[2]}
+    user_groups=${arr[3]}
+
+    echo "username=$username, passwd=$passwd, uid=$uid, user_groups=$user_groups"
+
+    IFS=','; read -ra groups <<< "$user_groups"; unset IFS
+    group_opt=""
+    for group in "${groups[@]}"; do
+        grep -q "^$group:" /etc/group || ( echo "Group $group for user $username doesn't exist"; exit 1 )
+        group_opt="$group_opt -G $group"
+    done
+
+    grep -q "^$username:" /etc/passwd || adduser -D -H $group_opt ${uid:+-u $uid} $username
+    echo -e "$passwd\n$passwd" | smbpasswd -s -a $username
+
+done
+
+ionice -c 3 smbd -FS --no-process-group < /dev/null
