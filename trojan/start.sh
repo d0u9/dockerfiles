50bd9fb7c78e417690d7728bb706cea106fac336
d0u9 <d0u9.su@outlook.com>
2021-05-06 22:12:50 +0800

    trojan update

--- trojan/start.sh
+++ trojan/start.sh
@@ -4,5 +4,5 @@ config=${CONFIG:-"/config/config.json"}
 addr=$(jq -r .remote_addr $config)
 port=$(jq -r .remote_port $config)
 
-python3 /usr/bin/getcert.py $addr $port | tee /config/cert.pem
+python3 /usr/bin/getcert.py $addr $port | tee /cert.pem
 su-exec trojan trojan -c "$config"
