e0d2b2f50da9354303d0f78c856433dad5563ae8
Douglas Su <d0u9.su@outlook.com>
2022-02-23 17:48:29 +0800

    haproxy

---
+++ haproxy/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PLATFORMS=""
+PLATFORMS+="linux/amd64"
+PLATFORMS+=","
+PLATFORMS+="linux/arm64"
+PLATFORMS+=","
+PLATFORMS+="linux/386"
+
+docker buildx build \
+    --push \
+    --platform $PLATFORMS \
+    -t d0u9/haproxy:latest \
+    .
--- haproxy/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-config=${CONFIG:-"/config/config.cfg"}
-
-su-exec haproxy haproxy -f "$config"
-
