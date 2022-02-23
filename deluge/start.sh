0c05305dd893c948a0a713723cf3c4177fa87248
Douglas Su <d0u9.su@outlook.com>
2022-02-23 17:13:36 +0800

    update deluge

--- deluge/start.sh
+++ deluge/start.sh
@@ -1,28 +1,57 @@
-#!/bin/bash
+#!/bin/sh
 
 PUID=${PUID:-0}
 PGID=${PGID:-0}
-UMASK=${UMASK:-$(umask)}
+UMASK=${UMASK:-0}
 
 # For the first run
 SEALED_OFF="/sealed_off"
 if [ ! -f $SEALED_OFF ]; then
     if [ "$PGID" != "0" ]; then
-        GROUP="deluge"
-        groupadd -g $PGID $GROUP
+        groupmod -g $PGID deluge
     fi
 
     if [ "$PUID" != "0" ]; then
-        USER="deluge"
-        useradd -m -s /sbin/nologin -u $PUID -g $PGID $USER
-        echo "umask $UMASK" >> /home/$USER/.bashrc
-    else
-        echo "umask $UMASK" >> /root/.bashrc
+        usermod -u $PUID deluge
+    fi
+
+    if [ "$UMASK" != "0" ]; then
+        echo "umask $UMASK" >> /home/deluge/.bashrc
     fi
 
     touch $SEALED_OFF
 fi
 
-exec start-stop-daemon -S -c $PUID:$PGID -k $UMASK -x /usr/bin/deluged -- -d &
-exec gosu $PUID:$PGID deluge-web
+CONFIG=config
+LOG_LEVEL=warning
 
+su-exec deluge:deluge deluged \
+    --user deluge \
+    --group deluge \
+    --config $CONFIG \
+    --loglevel $LOG_LEVEL \
+    --port 58846 \
+    --pidfile $(pwd)/deluged.pid \
+    ;
+
+su-exec deluge:deluge deluge-web \
+    --user deluge \
+    --group deluge \
+    --config $CONFIG \
+    --loglevel $LOG_LEVEL \
+    --port 8112 \
+    --pidfile $(pwd)/deluge-web.pid \
+    ;
+
+KILLSIG=SIGTERM
+
+trap quit SIGTERM
+function quit() {
+    kill -s $KILLSIG $(cat $(pwd)/deluge-web.pid)
+    kill -s $KILLSIG $(cat $(pwd)/deluged.pid)
+
+    exit 0
+}
+
+
+while true; do sleep 3; done
